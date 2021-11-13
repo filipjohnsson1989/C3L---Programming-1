4c1b54d14912655ff3aa831eaa89955d850ea5e4
Filip Johnsson <filip.johnsson1989@gmail.com>
2021-11-14 00:10:43 +0100

    Add Person class

---
+++ UPG5/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UPG5
+{
+    class Person
+    {
+        private string _namn;
+        private int _alder;
+
+        public Person(string name, int alder)
+        {
+            _namn = name;
+            _alder = alder;
+        }
+
+        public string PersonInfo()
+        {
+            return "Hej " + _namn +
+                ", du är " + _alder + "år";
+        }
+    }
+}
