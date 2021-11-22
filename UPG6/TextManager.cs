4264d7d1ae69705f219ce38bbdf5004020fff043
Filip Johnsson <filip.johnsson1989@gmail.com>
2021-11-22 23:16:41 +0100

    Add Text manager class

---
+++ UPG6/TextManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UPG6
+{
+    class TextManager
+    {
+    }
+}
