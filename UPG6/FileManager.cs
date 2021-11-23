e96c3498cfcd53ebfd55c636eb784ae60dd7e792
Filip Johnsson <filip.johnsson1989@gmail.com>
2021-11-23 01:09:15 +0100

    Add Update method to create or open file and add text to it

--- UPG6/FileManager.cs
+++ UPG6/FileManager.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace UPG6
 {
-    class FileManager
+    static class FileManager
     {
+        private const string _path = "ProgramFile.txt";
+
+        public static void Update(string line)
+        {
+            using (StreamWriter sw = (File.Exists(_path)) ? File.AppendText(_path) : File.CreateText(_path))
+            {
+                    sw.WriteLine(line);
+            }
+
+        }
+
+        public static string Read()
+        {
+            string fileContent;
+            using (StreamReader sr = new StreamReader(_path))
+            {
+                fileContent = sr.ReadToEnd();
+            }
+
+            return fileContent;
+        }
     }
 }
