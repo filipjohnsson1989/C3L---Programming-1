62b3be96b46d97612b7c3974c2aae6fcc2b5ead8
Filip Johnsson <filip.johnsson1989@gmail.com>
2021-11-23 01:13:34 +0100

    Ask user for data to append to file and read them with using of File manager class to work with file and Text manager class for formating texts for both reading and writing.

--- UPG6/Program.cs
+++ UPG6/Program.cs
@@ -10,6 +10,25 @@ namespace UPG6
     {
         static void Main(string[] args)
         {
+            string input;
+            bool addNewLine;
+
+            Console.WriteLine(
+                    @"Hej!
+Skriv in texten som ska läggas till i filen, annars tryck på Enter för att avsluta!"
+                );
+
+
+            do
+            {
+                input = Console.ReadLine();
+                addNewLine = !string.IsNullOrEmpty(input);
+                if (addNewLine)
+                    FileManager.Update(TextManager.InputFormat(input));
+            } while (addNewLine);
+
+            Console.WriteLine("Läsa och skriva filen i storbostäver");
+            Console.WriteLine(TextManager.OutputFormat(FileManager.Read()));
         }
     }
 }
